f1016def5a95454278a5e02447613e33e80c2238
publius190 <nbroulidakis@gmail.com>
2018-12-02 13:20:39 -0500

    adding calcs and interpolation formula

--- main.js
+++ main.js
@@ -197,6 +197,130 @@ const shotData = {
   }
 }
 
+var shotCalc = function (pinDist, elevation, windSpeed, windAngle, ballEffect, ballBreaks, greenInc, shotType, club) {
+ 
+	let currShot = shotData.shotType.club
+	
+	//Distance addition due to ball effect
+	var terrainDist = shotData.shotType.terrainEffect["70"] + pinDistance;
+	var hWindVector = windSpeed * Math.sin(windAngle * Math.PI / 180); 
+	var vWindVector = windSpeed * Math.cos(windAngle * Math.Pi / 180);
+	
+	//find starting horizontal wind distance 
+	var hwi = xSquared**2 + xTerm + constant;
+	
+	//find the max power from hwi table for next calculation
+	var lastVal = currShot.distArray.length - 1
+	
+	//find the change in elevation effect due to reduction of max shot distance
+	if (elevation > 0) {
+		var eleFactor = currShot.eleEffectPos*Math.pow(currShot.eleVarPos,currShot.distArray[lastVal]-terrainDist);
+	} else {
+		var eleFactor = currShot.eleEffectNeg*Math.pow(currShot.eleVarNeg,currShot.distArray[lastVal]-terrainDist);
+	}
+	
+	//shot specific elevation factored into total elevation
+	var eleActual = eleFactor * elevation;
+	//influence due to the total force of the wind on shot aim
+	var windInfluence = hWindVector * hwi;
+	
+	//calculate effect of wind on shot distance based on head or tailwind and calculate final pin distance
+	if (windAngle<=90) {
+		var windDist = vWindVector*hwi*currShot.eleWindFactPos*(1-(eleActual*currShot.windDistFactPos));
+		var percentWindEffect = (100+(windDist*windFactorPos/-4))/100;
+		var finalPinDist = terrainDist+eleActual-windDist;
+	} else {
+		var windDist = vWindVector*hwi*currShot.eleWindFactNeg*(1-(eleActual*currShot.windDistFactNeg);
+		var percentWindEffect = (100+(windDist*4/-windFactorNeg))/100;
+		var finalPinDist = terrainDist+eleActual-windDist;
+	}
+	
+	//effect of slope on horizontal aiming
+	var slopeEffect = hwi / 4 * ballBreaks;		
+	
+	//total elevation influence on shot distance
+	if (elevation>=0) {						
+		var eleInfTotal = (100+(eleActual/-eleInfPos))/100;	
+	} else {
+		var eleinfTotal = (100+(eleActual/-eleInfNeg))/100;
+	}
+	
+	var finalHWI = hwi * hWindVector * eleInfTotal;
+	if(windAngle<=90) {
+		var horizontalUnits = finalHWI * percentWindEffect + slopeEffect;
+	} else {
+		var horizontalUnits = finalHWI / percentWindEffect + slopeEffect;
+	}
+	
+	//create array for shot% and power
+	var 
+	
+	//calculate final shot distance adjustments
+	var shotPower = interpolateArray(currShot.distArray, powerArray, finalPinDist);
+	var caliperDist = shotPower / 100 * maxShotPower;
+	
+	//convert horizontal unit measurement to power bar measurement
+	var pbDist = Math.mod(horizontalUnits,1.92)/0.217; 
+	
+	//this section calculates spin for dunk shots and provides recommended spins
+	//for tomahawks and backspin shots
+	if (shotType = 'dunk') {
+		var normal1 = Math.mod(shotPower, 30/108);
+		var normal2 = normal1 / (30/108);
+		if (normal2 < 0.288) {
+			var normal3 = Math.floor(normal2);
+		} else {
+			var normal3 = Math.ceil(normal2);
+		}
+		
+		if (normal1 > (30/108) - 0.08) {
+			var spinVariant = 9;
+		} else {
+			var spinVariant = 8;
+		}
+		
+		if (normal1 < 0.08) {
+			var spinActual = 9;
+		} else {
+			var spinActual = spinVariant;
+		}
+	} else if (shotType = 'tomahawk') {
+		var spinActual = 7;
+	} else {
+		var spinActual = 9;
+	}
+	
+	//return object containing all of the shot information to be displayed
+	return {
+		shotPower: shotPower,
+		caliperDist: caliperDist,
+		pbDist: pbDist,
+		spinActual: spinActual
+	};
+};
+
+
+//Function that interpolates a table that is passed to it within two arrays
+var interpolateArray = function(xArray, yArray, newX) {
+	var arrayLength = xArray.length;
+	var y1, y2, x1, x2, i, lowBounds;
+	
+	for (i = 0; i < arrayLength; i++) {
+		if (xArray[i] < newX) {
+			lowBounds = i;
+		}
+	}
+	
+	x1 = xArray[i];
+	y1 = yArray[i];
+	x2 = xArray[i+1];
+	y2 = yArray[i+1];
+	
+	var newY = y1 + (newX - x1)*(y2-y1)/(x2-x1); 
+
+	return newY;
+};
+
 function appendResult(text) {
   document.getElementById('result').innerHTML += text;
 }
